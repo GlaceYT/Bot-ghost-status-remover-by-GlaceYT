cd98f42344fb26223a1f53528b053a30a8ec0920
hollownest345 <157656535+hollownest345@users.noreply.github.com>
2024-01-24 20:38:47 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["estou disposto a ajudar","bom dia","tudo bem?"];
 
 
 let currentIndex = 0;
