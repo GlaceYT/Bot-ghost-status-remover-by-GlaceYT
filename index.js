14fc4e164545aadbad32751fff40d87d67d9a287
CommunistBendy <158279254+CommunistBendy@users.noreply.github.com>
2024-01-31 16:31:29 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["I Love Communisum☭"];
 
 
 let currentIndex = 0;
