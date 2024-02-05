40e79d7b839313eb2f3c7d96bcdf62d387006f88
UnknownMysteryOfficial <116437970+UnknownMysteryOfficial@users.noreply.github.com>
2024-02-04 21:39:53 -0800

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Watching Mortis Development","Watching Mortis Developmen","Watching Mortis Developmen"];
 
 
 let currentIndex = 0;
