344c6430e8c0c8ba0c653bbc304d900975798070
KnuckleP0pper <140670000+KnuckleP0pper@users.noreply.github.com>
2024-02-16 19:17:24 +0100

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Watching Netflix","Listening to Spotify","Playing GTA VI"];
+const statusMessages = ["Under your bed"];
 
 
 let currentIndex = 0;
