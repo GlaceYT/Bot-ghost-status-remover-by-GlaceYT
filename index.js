4febd91fe9db0eefe8867cb5e0107ebfbe907a4a
Glemn1 <113846823+Glemn1@users.noreply.github.com>
2024-02-24 12:35:04 +0200

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Watching over the server"];
 
 
 let currentIndex = 0;
