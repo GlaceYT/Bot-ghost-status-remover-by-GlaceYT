4d87e48ec2cbf0c80c338f94d731c64784ff846e
Fox-apk <83433705+Fox-apk@users.noreply.github.com>
2024-03-04 19:52:49 -0800

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Jugando con Richy, Jugando Mxrp"];
 
 
 let currentIndex = 0;
