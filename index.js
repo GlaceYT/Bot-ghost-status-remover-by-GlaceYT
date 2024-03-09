96331c029f7e9eb2d6771a1f03acdf642886f816
simonjnl <162840639+simonjnl@users.noreply.github.com>
2024-03-09 22:33:46 +0100

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = [".gg/celesticalservices"];
 
 
 let currentIndex = 0;
