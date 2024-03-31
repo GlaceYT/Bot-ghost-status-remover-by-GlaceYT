489fc1b8364cc76470f06fd402c5b75db3bc689e
Blackerbl <162997554+Blackerbl@users.noreply.github.com>
2024-03-31 17:30:30 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Sizi İzliyorum?","Sizinle Uğraşıyoum!"];
 
 
 let currentIndex = 0;
