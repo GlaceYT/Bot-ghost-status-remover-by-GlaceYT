cd149109f79a5d68cdd4e92be2fc09190f09e37f
OfficialHiraishi <165544569+OfficialHiraishi@users.noreply.github.com>
2024-03-30 23:34:14 -0700

    Hiraishi.js
    Hiraishi

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to .gg/hiraishi","Watching PornHub"];
 
 
 let currentIndex = 0;
