6703d781f48e020e70186e49f97cfdeba50fc6d1
zCero499 <166229570+zCero499@users.noreply.github.com>
2024-04-06 10:57:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -91,9 +91,9 @@ function updateStatusAndSendMessages() {
 }
 
 client.once('ready', () => {
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    Viendo /help | .gg/bnrs`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    Viendo /help | .gg/bnrs`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    Viendo /help | .gg/bnrs`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
