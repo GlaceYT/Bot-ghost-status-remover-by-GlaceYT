4b74be91a3532af366344418e9083710a1f550f6
Arunabho <140074831+DorachanCodes@users.noreply.github.com>
2024-04-14 00:02:39 +0530

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Listening to Spotify","Watching YouTube"];
+const statusMessages = ["Listening to Spotify","Watching YouTube", "Making Would You Rather Telegram Bot" , "Thinking About My Ex"];
 
 
 let currentIndex = 0;
