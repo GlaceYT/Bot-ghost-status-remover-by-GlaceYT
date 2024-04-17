e016acce7d5db4e4a86a8d09eba306598618858b
Blackerbl <162997554+Blackerbl@users.noreply.github.com>
2024-04-17 22:21:06 +0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Sizi İzliyorum?","Sizinle Uğraşıyoum!"];
+const statusMessages = ["İnsanlardan Nefret Ediyorum","Sizi İzliyorum?","Sizinle Uğraşıyoum!","Kuru Kuru Kururin!","Hey Dikkatli Ol Gözüm Üzerinde! ","Ne Bakıyon Rias var?"];
 
 
 let currentIndex = 0;
