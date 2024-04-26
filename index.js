9d7b027b6576718808c704e44f0cd7e1cd1db6ac
bykilleris <166397077+bykilleris@users.noreply.github.com>
2024-04-26 13:22:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["👑❤️"];
 
 
 let currentIndex = 0;
@@ -91,9 +91,9 @@ function updateStatusAndSendMessages() {
 }
 
 client.once('ready', () => {
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✅ Bot is ready as ${client.user.tag}`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ✨HAPPY NEW YEAR MY DEAR FAMILY`);
-  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️WELCOME TO 2024`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|     Bot is ready as ${client.user.tag}`);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    `);
+  console.log(`\x1b[36m%s\x1b[0m`, `|    ❤️`);
   updateStatusAndSendMessages();
 
   setInterval(() => {
