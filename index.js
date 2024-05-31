61b64895084e83e87a266048241c1ec2376f9461
chvendas <chronumvendas@gmail.com>
2024-05-30 23:43:54 -0300

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["Jogando Xvideos","Fazendo dinheiro","Money"];
+const statusMessages = ["Produtos de qualidade","O melhor do mercado","By hoko.xpn and kzn.xp"];
 
 
 let currentIndex = 0;
