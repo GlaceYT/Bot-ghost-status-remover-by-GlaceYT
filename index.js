1adf06b691a3fa728b6325a0c7e830b78ff2bd68
scooby2k <scoobyscooby862@gmail.com>
2024-06-03 11:16:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["המנהל הוא ריי","השרת הכי טוב: ray server" ,"כל הזכויות שמורות לדנה"];
 
 
 let currentIndex = 0;
@@ -74,7 +74,7 @@ function updateStatusAndSendMessages() {
 
   client.user.setPresence({
     activities: [{ name: currentStatus, type: ActivityType.Custom}],
-    status: 'dnd',
+    status: 'online',
   });
 
   
