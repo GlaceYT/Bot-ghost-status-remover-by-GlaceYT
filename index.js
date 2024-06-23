9072528f12abbfa38a7b6b158ff9e31e068f681f
OfficiallyA-Noob <officialwithnoob@gmail.com>
2024-06-23 14:25:28 +0000

    index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.listen(port, () => {
 });
 
 
-const statusMessages = ["PLAYING","MUSIC"];
+const statusMessages = ["Hello, I am Wilson, Official Infinity's butler!"];
 
 
 let currentIndex = 0;
