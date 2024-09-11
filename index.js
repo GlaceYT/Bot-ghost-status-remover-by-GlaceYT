197983ddfad117a352028c4507d3f3acbf2a1c63
SHIVAREDDY002 <157602459+SHIVAREDDY002@users.noreply.github.com>
2024-09-11 20:47:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
+const statusMessages = ["EDIT THE STATUS", "🎮 Playing VALORANT"];
 let currentIndex = 0;
 
 async function login() {
