905c749626d4337e2a56d6ea3b6614c28dcd1ecc
romo20203 <hfjddghrdf@gmail.com>
2024-10-11 17:42:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎧 Listening to Spotify", "🎮 Playing VALORANT"];
-const statusTypes = [ 'dnd', 'idle'];
+const statusMessages = ["My dev: dana2", "Vibe&chill the best!:"];
+const statusTypes = [ 'online', 'online'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
 
