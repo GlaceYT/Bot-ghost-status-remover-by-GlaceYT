411eed5a89fac7aceaf85ec295c5f3e96d7a0b06
Mahadgaming5349 <162704755+MAR-Official-YT@users.noreply.github.com>
2024-11-29 22:59:37 +0600

    Status_index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@
 /_______  /\___|_  /|___|  \___/\____|__  / 
         \/       \/                     \/  
                     
-DISCORD :  https://discord.com/invite/xQF9f9yUEM                   
-YouTube : https://www.youtube.com/@GlaceYT                         
+DISCORD :  https://disc.gg/modding-us                   
+YouTube : https://www.youtube.com/@mahadgaming5349                         
                                                                        
 ☆.。.:*・°☆.。.:*・°☆.。.:*・°☆.。.:*・°☆
 
@@ -37,7 +37,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', '\x1b[32m SH : http://localhost:' + port + ' ✅\x1b[0m');
 });
 
-const statusMessages = ["🎮 Playing Minecraft"];
+const statusMessages = ["🎮 activity stuff"];
 const statusTypes = [ 'dnd'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
